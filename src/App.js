edec06d0342630df41cd72e66c3f2a185321651b
Abhinav-Shekhar <abhirocksnav@gmail.com>
2021-11-05 20:00:04 +0530

    added Recipe.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 function App() {
@@ -6,16 +7,18 @@ function App() {
   const API_ID = "49338b8c";
   const API_KEY = "e178173bda65a57f940f8f104509f39c";
 
+  const [recipes, setRecipes] = useState([]);
+
   useEffect(() => {
     getRecipes();
   }, []);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}`
     );
     const data = await response.json();
-    console.log(data.hits);
+    setRecipes(data.hits);
   };
 
   return (
@@ -24,6 +27,13 @@ function App() {
         <input className="search-bar" type="text"></input>
         <button className="search-button" type="submit">Search</button>
       </form>
+      {recipes.map(recipe => (
+        <Recipe 
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 }
