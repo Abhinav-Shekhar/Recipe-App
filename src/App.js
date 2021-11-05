5f709a12fe289abb625e9a35d839e1b4ae152f93
Abhinav-Shekhar <abhirocksnav@gmail.com>
2021-11-05 19:20:30 +0530

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const API_ID = "49338b8c";
+  const API_KEY = "e178173bda65a57f940f8f104509f39c";
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.hits);
+  };
+
   return (
     <div className="App">
-      <h1>Hello React!!</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"></input>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
